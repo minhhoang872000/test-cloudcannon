a8443ee30257f22a7d2b45fe4a60b3620a635b74
Nguyen Nguyen <manhnd@dtsmart.vn>
2023-11-09 15:12:44 +0000

    Updated 1 file via CloudCannon.

--- components/blog-layout.vue
+++ components/blog-layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container ">
         <div class="row">
             <div class="col-lg-4 col-md-6" v-for="post in posts">
                 <article class="blog-post">
